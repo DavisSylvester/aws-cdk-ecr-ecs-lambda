e573de7ef2ea0ad85aef04b0329a5dae8ba1b8a1
Davis Sylvester III <davis@sylvesterllc.com>
2022-06-06 23:40:01 -0500

    Initial commit

---
+++ src/AwsCdkEcrEcs/AwsCdkEcrEcsStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace AwsCdkEcrEcs
+{
+    public class AwsCdkEcrEcsStack : Stack
+    {
+        internal AwsCdkEcrEcsStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
